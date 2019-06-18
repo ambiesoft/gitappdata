f0506b49b80b36a09a492c8b3ac829aaf80129a2
ambiesoft <ambiesoft.trueff@gmail.com>
2019-06-18 12:43:07 +0900

    bitbucket

--- gitappdata.js
+++ gitappdata.js
@@ -2,37 +2,52 @@
 //     const config = require('gitappdata.config')
 // }
 // console.log("cwd = " + process.cwd())
+function dlog(s) {
+    if (Debugging) {
+        console.log(s)
+    }
+}
+function toArray(o) {
+    var tmp = []
+    for (v of o) {
+        tmp.push(v)
+    }
+    return tmp
+}
+global.btoa = function (b) {
+    return Buffer.from(b, 'base64').toString()
+}
+
 
 var config
-if ( process.argv === undefined || process.argv.length <= 2) {
+if (process.argv === undefined || process.argv.length <= 2) {
     config = require('./config/default.json')
 } else {
     // could not use 'require' outside of source directory
     var fs = require('fs');
     config = JSON.parse(fs.readFileSync(process.argv[2], 'utf8'));
 }
-// console.log(config)
-// console.log(Array.isArray(config))
-var tmp = []
-for( v of config) {
-    tmp.push(v)
-}
-config = tmp
+config = toArray(config)
 
+// GitHub API interface 
 const Octokit = require('@octokit/rest')
 const octokit = new Octokit()
+
+// Bitbucket API interface
+const Bitbucket = require('bitbucket')
+const clientOptions = {
+    baseUrl: 'https://api.bitbucket.org/2.0',
+    headers: {},
+    options: {
+        timeout: 1000
+    }
+}
+const bitbucket = new Bitbucket(clientOptions)
+
+
 const AppInfo = require('./appinfo')
 const Debugging = false
 
-function dlog(s) {
-    if (Debugging) {
-        console.log(s)
-    }
-}
-
-global.btoa = function (b) {
-    return Buffer.from(b, 'base64').toString()
-}
 function getDateFromHistory(history) {
     history = btoa(history)
     var line = history.split(/\r?\n/g)[0]
@@ -43,8 +58,9 @@ function main() {
     var dupnamecheck = []
     var promises = config.map(function (v) {
 
-        if (v.repotype !== 'github') {
-            throw 'repotype must be github';
+        if (v.repotype !== 'github' &&
+            v.repotype !== 'bitbucket') {
+            throw 'repotype must be "github" or "bitbucket"';
         }
 
         if (dupnamecheck.includes(v.owner + v.name)) {
@@ -55,25 +71,39 @@ function main() {
         let appinfo = new AppInfo(v.name, v.owner);
         appinfo.setOnelineDesc(v.onelinedesc)
 
-        return octokit.repos.getContents({
-            owner: v.owner,
-            repo: v.name,
-            path: v.history
-        }).then(({ data }) => {
-            dlog(btoa(data.content))
-            appinfo.setHistory(data.content)
-            appinfo.setDate(getDateFromHistory(data.content))
-            return octokit.repos.listTags({
+        if (v.repotype === 'github') {
+            return octokit.repos.getContents({
                 owner: v.owner,
                 repo: v.name,
+                path: v.history
             }).then(({ data }) => {
-                dlog(data)
-                appinfo.setVersion(data[0].name)
-                appinfos.push(appinfo)
+                dlog(btoa(data.content))
+                appinfo.setHistory(data.content)
+                appinfo.setDate(getDateFromHistory(data.content))
+                return octokit.repos.listTags({
+                    owner: v.owner,
+                    repo: v.name,
+                }).then(({ data }) => {
+                    dlog(data)
+                    appinfo.setVersion(data[0].name)
+                    appinfos.push(appinfo)
+                })
             })
-        })
-
-
+        } else if (v.repotype === 'bitbucket') {
+            if (v.username) {
+                bitbucket.authenticate({
+                    type: 'basic',
+                    username: v.username,
+                    password: v.password
+                })
+            }
+            return bitbucket.repositories
+                .list({ username: v.owner })
+                .then(({ data, headers }) => {
+                    console.log(data.values)
+                })
+                .catch(err => console.error(err))
+        }
     })
 
     Promise.all(promises).then((_) => {
