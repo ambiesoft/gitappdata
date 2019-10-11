d59c540dad12f274e29db4ef13ea2947bf2ebcfc
ambiesoft <ambiesoft.trueff@gmail.com>
2019-10-11 12:24:56 +0900

    moon and utf8

--- getgitappdata.js
+++ getgitappdata.js
@@ -1,3 +1,5 @@
+const utf8 = require('utf8');
+
 let DEBUGGING = false;
 function dtrace(message) {
     if(DEBUGGING) {
@@ -16,21 +18,36 @@ function toArray(o) {
 }
 
 var config
+var outputfile
 if (process.argv === undefined || process.argv.length <= 2) {
     config = require('./config/default.json')
 } else {
     // could not use 'require' outside of source directory
     var fs = require('fs');
     config = JSON.parse(fs.readFileSync(process.argv[2], 'utf8'));
+    if(process.argv[3]==='-o') {
+        outputfile = process.argv[4]
+    }
+
+    // check
+    if(!outputfile) {
+        console.log('No output file specified')
+        return
+    }
 }
 config = toArray(config)
+
+
 
 
 let gitappdata = new GitAppData();
 gitappdata.get(config)
 .then(promises => {
     Promise.all(promises).then((appinfos) => {
-        console.log(JSON.stringify(appinfos))
+        const str = JSON.stringify(appinfos)
+        var fs = require('fs');
+        fs.writeFileSync(outputfile, str)
+        // console.log(JSON.stringify(appinfos))
     })
 
 })
